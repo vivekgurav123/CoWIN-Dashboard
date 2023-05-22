589b79278d4596e158bda2b3d628fe2c228be795
Vivek Gurav <113456559+vivekgurav123@users.noreply.github.com>
2023-05-22 10:39:11 +0530

    second commit

--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -4,8 +4,7 @@ import {
   Bar,
   XAxis,
   YAxis,
-  Legend,
-  ResponsiveContainer,
+  Legend
 } from 'recharts'
 
 const VaccinationCoverage = props => {
@@ -22,13 +21,11 @@ const VaccinationCoverage = props => {
   return (
     <div>
       <h1>Vaccination Coverage</h1>
-      <>
-        <ResponsiveContainer width={1000} height={300}>
           <BarChart
             data={vaccinationCoverageData}
             margin={{
               top: 5,
-            }}
+            }} width={1000} height={300}
           >
             <XAxis
               dataKey="vaccine_date"
@@ -52,8 +49,6 @@ const VaccinationCoverage = props => {
             <Bar dataKey="dose_1" name="Dose 1" fill="#2d87bb" barSize="20%" />
             <Bar dataKey="dose_2" name="Dose 2" fill="#f54394" barSize="20%" />
           </BarChart>
-        </ResponsiveContainer>
-      </>
     </div>
   )
 }
